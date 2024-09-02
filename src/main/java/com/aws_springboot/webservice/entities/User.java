1252d62f664b0eba2a5ee66d73d0d0b96be7f506
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-02 17:34:33 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/aws_springboot/webservice/entities/User.java
+++ src/main/java/com/aws_springboot/webservice/entities/User.java
@@ -1,12 +1,14 @@
 package com.aws_springboot.webservice.entities;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +23,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders;
 	
 	public User() {
 	}
