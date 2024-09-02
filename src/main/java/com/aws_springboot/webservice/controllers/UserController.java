6df781a238ab32ca55c8a581732ddc7ed83085f7
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-02 17:02:14 -0300

    Service layer, component registration

--- src/main/java/com/aws_springboot/webservice/controllers/UserController.java
+++ src/main/java/com/aws_springboot/webservice/controllers/UserController.java
@@ -1,20 +1,33 @@
 package com.aws_springboot.webservice.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aws_springboot.webservice.entities.User;
+import com.aws_springboot.webservice.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1, "Bob Brown", "bob@gmail.com", "(15) 99200-4567", "1234567");
-		return ResponseEntity.ok(user);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok(obj);
+	}
 }
---
+++ src/main/java/com/aws_springboot/webservice/services/UserService.java
@@ -0,0 +1,27 @@
+package com.aws_springboot.webservice.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aws_springboot.webservice.entities.User;
+import com.aws_springboot.webservice.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		List<User> list = userRepository.findAll();
+		return list;
+	}
+	
+	public User findById(Integer id) {
+		User obj = userRepository.findById(id).get();
+		return obj;
+	}
+	
+}
