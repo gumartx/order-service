106f3fa7ae0a61003223484f0ff5d56a1fa3686e
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-03 15:35:54 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/aws_springboot/webservice/entities/Product.java
+++ src/main/java/com/aws_springboot/webservice/entities/Product.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +37,9 @@ public class Product implements Serializable {
 			inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Product() {
 	}
 
@@ -86,6 +93,11 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		return items.stream().map(x -> x.getOrder()).collect(Collectors.toSet());
 	}
 
 	@Override
